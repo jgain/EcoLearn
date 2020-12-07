ee74583c17df39df3c1ee2b4bd821571fcbeffc3
kkapp <konrad.p.kapp@gmail.com>
2020-12-07 15:48:11 +0200

    DB generation now uses relative paths

--- data_preproc/common_data/regen.sh
+++ data_preproc/common_data/regen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ~/PhDStuff/prototypes/repo/code/py_scripts/create_sql_db.py ~/EcoSynth/data_preproc/common_data
+python3 ~/PhDStuff/prototypes/repo/code/py_scripts/create_sql_db.py ./
 
-cp ~/EcoSynth/data_preproc/common_data/sonoma.db ~/EcoSynth/ecodata/sonoma.db
+mv ./sonoma.db ../../EcoSynth/ecodata/sonoma.db
